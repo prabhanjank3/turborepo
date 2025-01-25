5599b0d28ec644355f4a59e35db19a5e18702a19
pkulkarni5 <pkulkarni5@atlassian.com>
2025-01-25 17:23:28 +0530

    Added mui support to test app

--- apps/template/src/index.tsx
+++ apps/template/src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
+import { theme as muiTheme } from './theme';
 import ReactDOM from 'react-dom';
 import App from './app';
 
+const theme = createTheme(muiTheme);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
   document.getElementById('root')
 );
