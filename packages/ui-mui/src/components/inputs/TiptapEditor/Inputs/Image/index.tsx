84521a16fd857030e88b0d7ec344d5ac1dbd1a95
pkulkarni5 <pkulkarni5@atlassian.com>
2025-01-25 12:31:14 +0530

    Skipped incomplete implementation as blocking due to typescript errors

--- packages/ui-mui/src/components/inputs/TiptapEditor/Inputs/Image/index.tsx
+++ packages/ui-mui/src/components/inputs/TiptapEditor/Inputs/Image/index.tsx
@@ -1,31 +1,31 @@
-import React, { useState } from 'react';
-import { Box, Tab, Tabs } from '@mui/material';
-import ImageKitInput from '../../../Imagekit';
-import ImageLink from '../../../ImageLink';
+// import React, { useState } from 'react';
+// import { Box, Tab, Tabs } from '@mui/material';
+// import ImageKitInput from '../../../Imagekit';
+// import ImageLink from '../../../ImageLink';
 
-const DynamicTabs = ({ onSubmit }) => {
-  const [activeTab, setActiveTab] = useState(0);
+// const DynamicTabs = ({ onSubmit }) => {
+//   const [activeTab, setActiveTab] = useState(0);
 
-  const handleChange = (event, newValue) => {
-    setActiveTab(newValue);
-  };
+//   const handleChange = (event, newValue) => {
+//     setActiveTab(newValue);
+//   };
 
-  return (
-    <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
-      <Tabs value={activeTab} onChange={handleChange}>
-        <Tab label="ImageKit" />
-        <Tab label="Image Link" />
-      </Tabs>
-      {activeTab === 0 ? (
-        <ImageKitInput
-          onSuccess={onSubmit}
-          onError={err => window.alert('Something went wrong: ' + err)}
-        />
-      ) : (
-        <ImageLink name="image-link" label="Image Link" onChange={onSubmit} />
-      )}
-    </Box>
-  );
-};
+//   return (
+//     <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+//       <Tabs value={activeTab} onChange={handleChange}>
+//         <Tab label="ImageKit" />
+//         <Tab label="Image Link" />
+//       </Tabs>
+//       {activeTab === 0 ? (
+//         <ImageKitInput
+//           onSuccess={onSubmit}
+//           onError={err => window.alert('Something went wrong: ' + err)}
+//         />
+//       ) : (
+//         <ImageLink name="image-link" label="Image Link" onChange={onSubmit} />
+//       )}
+//     </Box>
+//   );
+// };
 
-export default DynamicTabs;
+// export default DynamicTabs;
