161b612026334b9303141bb3569c042ee73353f3
pkulkarni5 <pkulkarni5@atlassian.com>
2025-01-26 13:49:22 +0530

    Added redux toolkit setup

--- apps/template/src/index.tsx
+++ apps/template/src/index.tsx
@@ -3,16 +3,20 @@ import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
 import { theme as muiTheme } from './theme';
 import ReactDOM from 'react-dom';
+import { store } from './store';
+import { Provider } from 'react-redux';
 import App from './app';
 
 const theme = createTheme(muiTheme);
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>,
   document.getElementById('root')
 );
