bc8ff5ba33ddcb0893e55aa738db9797a02dd1c9
pkulkarni5 <pkulkarni5@atlassian.com>
2025-01-31 15:31:23 +0530

    Added dynamic form as default export

--- packages/ui-mui/src/index.tsx
+++ packages/ui-mui/src/index.tsx
@@ -1,3 +1,5 @@
 export { default as CustomButton } from './components/button';
 export { default as Navbar } from './components/header';
 export { default as Footer } from './components/footer';
+export { default as DynamicForm } from './components/dynamicForm';
+export { type FormConfig } from './components/dynamicForm/types/formConfig';
