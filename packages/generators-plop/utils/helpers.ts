f571e2c3591e0adec14ea00d24af3a2c19524248
pkulkarni5 <pkulkarni5@atlassian.com>
2025-04-24 16:45:00 +0530

    helper for prompt

--- packages/generators-plop/utils/helpers.ts
+++ packages/generators-plop/utils/helpers.ts
@@ -1,3 +1,12 @@
+import fs from 'fs';
+import path from 'path';
+
+export const listDirectories = (basePath: string) => {
+  return fs
+    .readdirSync(basePath)
+    .filter((file) => fs.statSync(path.join(basePath, file)).isDirectory());
+};
+
 export const openCurlyHelper = () => {
   return '{';
 };
